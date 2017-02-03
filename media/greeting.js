70da75a63878b9e280878f5ee131a418617eb0fd
rebecca mcgrath <rebeccakmcgrath@gmail.com>
2017-02-02 18:39:50 -0600

    boolean

--- media/greeting.js
+++ media/greeting.js
@@ -1,24 +1,33 @@
-var name = prompt('Hello! What is your name?');
+var firstName = prompt('Hello! What is your first name?')|| "Visitor";
+var lastName = prompt('What is your last name?')|| "McDefaultson";
+
 var output = document.querySelector('#greeting');
-output.innerHTML = "<p>Thanks for visiting, " + name + ".</p>";
 
 
-var richardFood = "sushi";
-var simoneFood = "Pho";
-var genaFood = "pasta";
-
-var myFoodArray = ["Tacos", "Burriotos", richardFood, "curry", simoneFood, genaFood];
-
-var classCity =
-{
-  "gena": "Ashland", "rebecca": "Memphis", "wayne": "Manchester", "richard": "Galveston", "simone": "Louisville"
+if(firstName && lastName) {
+  output.innerHTML = "<p>Thanks for visiting, " + firstName + lastName + ".</p>";
+} else {
+  output.innerHTML ="<p>Please tell us your name!</p>";
 }
 
-var cityOutput = document.querySelector('#myCity');
-cityOutput.innerHTML = "<p>" + classCity.rebecca + "</p>";
 
-var foodOutput = document.querySelector('#favFood');
-foodOutput.innerHTML = "<p>" + myFoodArray[0] + "</p>";
+
+// var richardFood = "sushi";
+// var simoneFood = "Pho";
+// var genaFood = "pasta";
+//
+// var myFoodArray = ["Tacos", "Burriotos", richardFood, "curry", simoneFood, genaFood];
+//
+// var classCity =
+// {
+//   "gena": "Ashland", "rebecca": "Memphis", "wayne": "Manchester", "richard": "Galveston", "simone": "Louisville"
+// }
+//
+// var cityOutput = document.querySelector('#myCity');
+// cityOutput.innerHTML = "<p>" + classCity.rebecca + "</p>";
+//
+// var foodOutput = document.querySelector('#favFood');
+// foodOutput.innerHTML = "<p>" + myFoodArray[0] + "</p>";
 
 
 
